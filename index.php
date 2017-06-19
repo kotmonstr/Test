e6aa9a19ae1de504783e96eb3743a0df99d4464a
Konstantin Sychev <konstantin.s@wecandevelopit.com>
2017-06-19 17:54:40 +0300

    sql

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php echo "wewe"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+
+
+</head>
+<body>
+
+
+
+<form method="post" action="/register.php">
+    <label>Name</label>
+    <input type="text">
+    <br>
+
+    <label>Login</label>
+    <input type="text">
+    <br>
+
+    <label>Password</label>
+    <input type="text">
+    <br>
+
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
